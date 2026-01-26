2340c3cd5cd941a03a9a082d660da73f8f9c3522
Austin Tran <austintran3821@gmail.com>
2026-01-27 01:34:49 +1100

    Angular Skeleton Initiated + Add CORS to API

--- API/Program.cs
+++ API/Program.cs
@@ -11,10 +11,14 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {//use Sqlite as database provider
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors(); //allow secure and controlled access to your resources from different domains (origins), while still adhering to the browser's built-in security model, the same-origin policy
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().
+WithOrigins("http://localhost:4200","https://localhost:4200"));
 
 app.MapControllers();
 
